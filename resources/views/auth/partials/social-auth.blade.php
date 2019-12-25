3d17cddad691a80e961e90b21f053f010a4080fe
Osama Hamed <osamahamedmohammed@gmail.com>
2019-12-25 06:45:58 +0200

    update README.md

--- resources/views/auth/partials/social-auth.blade.php
+++ resources/views/auth/partials/social-auth.blade.php
@@ -3,5 +3,4 @@
 <div class="form-group text-center">
 	<a href="{{ route('social.auth.redirect', 'google') }}" class="btn btn-light w-25 mr-3"><i class="fa fa-google"></i> Google</a>
 	<a href="{{ route('social.auth.redirect', 'facebook') }}" class="btn btn-light w-25 mr-3"><i class="fa fa-facebook-official"></i> Facebook</a>
-	<button class="btn btn-light w-25"><i class="fa fa-twitter"></i> Twitter</button>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,12 @@
 @section('content')
 	<div class="card col-lg-6 offset-lg-3 mt-4 p-0">
 		<div class="card-body">
-			Welcome to the home page, {{ auth()->user()->name }}.
+			<p class="text-center">Welcome to the home page.</p>
+
+			<ul>
+				<li>Name: {{ auth()->user()->name }}</li>
+				<li>Email: {{ auth()->user()->email }}</li>
+			</ul>
 		</div>
 	</div>
 @endsection
